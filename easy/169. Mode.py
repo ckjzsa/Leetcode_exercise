6b87cce680529862a296031319e454f0ea51a6bd
Kehua Chen <48197101+ckjzsa@users.noreply.github.com>
2019-10-29 14:32:35 +0800

    Update 169. Mode.py

--- easy/169. Mode.py
+++ easy/169. Mode.py
@@ -1,5 +1,7 @@
+# Time complexity is O(n)
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         for num in nums:
             if nums.count(num) > len(nums) / 2:
                 return num
+       
