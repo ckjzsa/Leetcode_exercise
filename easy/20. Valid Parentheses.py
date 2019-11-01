9e432d22436b49db1396c54233f7ff11d347924e
Kehua Chen <48197101+ckjzsa@users.noreply.github.com>
2019-11-01 17:26:47 +0800

    Update 20. Valid Parentheses.py

--- easy/20. Valid Parentheses.py
+++ easy/20. Valid Parentheses.py
@@ -9,3 +9,43 @@ class Solution(object):
             s = s.replace('[]', '')
             s = s.replace('()', '')
         return s == ''
+
+    # Stack based solution
+    class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        if len(s) == 0:
+            return True
+
+        for i in s:
+            if i == '(' or i == '[' or i == '{':
+                stack.append(i)
+
+            elif i == ')':
+                if len(stack) == 0:
+                    return False
+                elif stack[-1] != '(':
+                    return False
+                else:
+                    stack.pop()
+            elif i == ']':
+                if len(stack) == 0:
+                    return False
+                elif stack[-1] != '[':
+                    return False
+                else:
+                    stack.pop()
+            elif i == '}':
+                if len(stack) == 0:
+                    return False
+                elif stack[-1] != '{':
+                    return False
+                else:
+                    stack.pop()
+        return len(stack) == 0
+
+    
