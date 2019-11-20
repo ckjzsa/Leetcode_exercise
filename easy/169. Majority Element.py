4c402e5c8afd1bcd237c515d7d6f8bbf94cbcaa6
Kehua Chen <48197101+ckjzsa@users.noreply.github.com>
2019-11-20 14:45:55 +0800

    Update 169. Majority Element.py

--- easy/169. Majority Element.py
+++ easy/169. Majority Element.py
@@ -5,3 +5,25 @@ class Solution:
             if nums.count(num) > len(nums) / 2:
                 return num
        
+# correct answer, O(nlogn)
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = sorted(nums)
+        count = 1
+        if len(nums) == 1:
+            return nums[0]
+        else:
+            for i in range(1, len(nums)):
+                if nums[i - 1] == nums[i]:
+                    count += 1
+
+                if count > len(nums) / 2:
+                    return nums[i]
+            
+    
+    
+    
