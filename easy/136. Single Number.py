97b70462712b9ddc0f42c7a31e4668b926be580a
Kehua Chen <48197101+ckjzsa@users.noreply.github.com>
2020-02-26 20:27:24 +0800

    Update 136. Single Number.py

--- easy/136. Single Number.py
+++ easy/136. Single Number.py
@@ -5,4 +5,22 @@ class Solution:
             a ^= i
         
         return a
+
+    
+    
+    
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        else:
+            set_ = set(nums)
+            dict_ = {}
+            for i in set_:
+                dict_[str(i)] = 0
             
+            for i in nums:
+                dict_[str(i)] += 1
+            
+            return int(min(dict_, key=dict_.get))
