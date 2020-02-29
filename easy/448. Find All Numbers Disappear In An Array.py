e206e5a6f0364f70b39aa3ec78280a19e7a9a6b8
Kehua Chen <48197101+ckjzsa@users.noreply.github.com>
2020-02-29 10:09:27 +0800

    Update 448. Find All Numbers Disappear In An Array.py

--- easy/448. Find All Numbers Disappear In An Array.py
+++ easy/448. Find All Numbers Disappear In An Array.py
@@ -9,3 +9,20 @@ class Solution(object):
             nums[index] = -abs(nums[index])
         
         return [i + 1 for i, num in enumerate(nums) if num > 0]
+
+
+    
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
+
+        set_num = set(nums)
+        return_num = []
+        for num in range(1, len(nums) + 1):
+            if num not in set_num:
+                return_num.append(num)
+        
+        return return_num
+
+    
