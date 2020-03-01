393e9775a8b50aea9885322e26e30bc87e124705
Kehua Chen <48197101+ckjzsa@users.noreply.github.com>
2020-03-01 14:16:35 +0800

    Update 121. Best Time to Buy and Sell Stock.py

--- easy/121. Best Time to Buy and Sell Stock.py
+++ easy/121. Best Time to Buy and Sell Stock.py
@@ -1,22 +1,15 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        if len(prices) <= 1:
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) == 0 or len(prices) == 1:
             return 0
 
-        difference = [0 for _ in range(len(prices) - 1)]
+        delta = [0 for _ in range(len(prices) - 1)]
         for i in range(len(prices) - 1):
-            difference[i] = prices[i + 1] - prices[i]
+            delta[i] = prices[i+1] - prices[i]
         
-        cummulated_profit = [0 for _ in range(len(prices) - 1)]
-        cummulated_profit[0] = max(0, difference[0])
-        maxProfit = cummulated_profit[0]
+        cummu_pro = [0 for _ in range(len(prices) - 1)]
+        cummu_pro[0] = max(0, delta[0])
+        for i in range(1, len(cummu_pro)):
+            cummu_pro[i] = max(0, cummu_pro[i-1] + delta[i])
 
-        for i in range(1, len(prices) - 1):
-            cummulated_profit[i] = max(0, cummulated_profit[i - 1] + difference[i])
-            maxProfit = max(maxProfit, cummulated_profit[i])
-        
-        return maxProfit
+        return max(cummu_pro)
