6dcff8c61234dfa532640f2b3e9e75bf10dc64fc
Kehua Chen <48197101+ckjzsa@users.noreply.github.com>
2020-03-03 16:29:05 +0800

    Update 1. Two Sum.py

--- easy/1. Two Sum.py
+++ easy/1. Two Sum.py
@@ -5,3 +5,17 @@ class Solution:
             for k in range(i+1, len(nums)):
                 if nums[i] + nums[k] == target:
                     return [i, k]
+
+
+# O(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for index, num in enumerate(nums):
+            temp = target - num
+            if temp in hashmap:
+                return [hashmap[temp], index]
+            
+            hashmap[num] = index
+        
+        return None
