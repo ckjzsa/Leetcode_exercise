2458bca4cfb66a9168520ad0d22634caa747c2ba
Kehua Chen <48197101+ckjzsa@users.noreply.github.com>
2020-03-05 16:09:28 +0800

    Update 581. Shortest Unsorted Continuous Subarray.py

--- easy/581. Shortest Unsorted Continuous Subarray.py
+++ easy/581. Shortest Unsorted Continuous Subarray.py
@@ -1,26 +1,18 @@
-class Solution(object):
-    def findUnsortedSubarray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        start, end = 0, 0
-        id_start, id_end = 0, 0
-        nums_sorted = sorted(nums)
-        j = -1
-        for i in range(len(nums)):
-            if id_start == 0:
-                if nums[i] != nums_sorted[i]:
-                    start = i
-                    id_start += 1
-            if id_end == 0:
-                if nums[j] != nums_sorted[j]:
-                    end = j
-                    id_end += 1
-
-            j = j - 1
-
-        if end == 0:
+class Solution:
+    def findUnsortedSubarray(self, nums: List[int]) -> int:
+        index = []
+        if len(nums) == 1:
             return 0
 
-        return len(nums) + end + 1 - start
+        nums_sort = sorted(nums)
+        for i in range(len(nums)):
+            if nums[i] != nums_sort[i]:
+                index.append(i)
+        
+        if len(index) == 0:
+            return 0
+
+        minimum = min(index)
+        maximum = max(index)
+
+        return maximum - minimum + 1
