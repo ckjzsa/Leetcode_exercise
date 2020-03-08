9ab0240e01c6e11d07ebbebbf6e59b1c833205b1
Kehua Chen <48197101+ckjzsa@users.noreply.github.com>
2020-03-08 15:55:44 +0800

    Update 557. Reverse Words in a String.py

--- easy/557. Reverse Words in a String.py
+++ easy/557. Reverse Words in a String.py
@@ -13,3 +13,38 @@ class Solution(object):
                     reverse = reverse + stack.pop()
         
         return reverse[1:]
+
+# 独创方法
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        count = 0
+        dict = {}
+        # 统计空格数
+        for i in s:
+            if i == ' ':
+                dict[int(count)] = []
+                count += 1
+        
+        dict[int(count)] = []
+
+        # 每个单词单独记录
+        count = 0
+        for i in s:
+            if i != ' ':
+                dict[int(count)].append(i)
+            elif i == ' ':
+                count += 1
+
+        string = ""
+
+        for i in range(count+1):
+            for word in dict[int(i)][::-1]:
+                string += word
+            string += ' '
+        
+        return string[:-1]
+
+        
+        
+
+
