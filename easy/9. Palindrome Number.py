26bb2221c3e7277b332b180a93a003d436070e08
Kehua Chen <48197101+ckjzsa@users.noreply.github.com>
2020-03-10 13:44:43 +0800

    Update 9. Palindrome Number.py

--- easy/9. Palindrome Number.py
+++ easy/9. Palindrome Number.py
@@ -14,3 +14,27 @@ class Solution(object):
         
         return True
 
+    
+    
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        
+        x = str(x)
+        p1 = 0
+        p2 = -1
+
+        while p1 <= (len(x) + p2):
+            if x[p1] != x[p2]:
+                return False
+            
+            p1 += 1
+            p2 -= 1
+        
+        return True
+
