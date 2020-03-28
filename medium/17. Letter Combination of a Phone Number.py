9287fd5b0aa6a4d81e784d63bed1dd3bc551c95b
Kehua Chen <48197101+ckjzsa@users.noreply.github.com>
2020-03-28 10:16:53 +0800

    Create 17. Letter Combination of a Phone Number.py

---
+++ medium/17. Letter Combination of a Phone Number.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        phone = {'2': ['a', 'b', 'c'],
+                 '3': ['d', 'e', 'f'],
+                 '4': ['g', 'h', 'i'],
+                 '5': ['j', 'k', 'l'],
+                 '6': ['m', 'n', 'o'],
+                 '7': ['p', 'q', 'r', 's'],
+                 '8': ['t', 'u', 'v'],
+                 '9': ['w', 'x', 'y', 'z']}
+
+        def backtrack(combination, digits):
+            if len(digits) == 0:
+                res.append(combination)
+                return 
+            
+            for letter in phone[digits[0]]:
+                backtrack(combination + letter, digits[1:])
+        
+        res = []
+        if digits:
+            backtrack("", digits)
+        
+        return res
