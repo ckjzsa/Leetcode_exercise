21c36f108243b3fa9637508fd4eea86f3755bbe3
Kehua Chen <48197101+ckjzsa@users.noreply.github.com>
2020-06-25 11:40:17 +0800

    Update 24. 反转列表.py

--- 剑指offer/24. 反转列表.py
+++ 剑指offer/24. 反转列表.py
@@ -22,3 +22,30 @@ class Solution(object):
             head = temp
 
         return new
+
+
+# 递归法
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        def helper(last, cur):
+            if cur is None: 
+                return cur
+            next = cur.next
+            cur.next = last
+            
+            if next is None: 
+                return cur
+            
+            return helper(cur, next)
+
+        return helper(None, head)
