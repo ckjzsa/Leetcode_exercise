751e24f3e82d3a7eedc55214aec6211bdef940a1
Kehua Chen <48197101+ckjzsa@users.noreply.github.com>
2020-07-05 21:06:43 +0800

    Create **59. 滑动窗口的最大值.py

---
+++ 剑指offer/**59. 滑动窗口的最大值.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        deque = []
+        result = [] # deque也可以用collection里的双端队列实现
+        for i in range(0, len(nums)):
+            while deque and nums[i] > nums[deque[-1]]: # 只存有可能成为最大值的数字的index进deque
+                deque.pop()
+            deque.append(i)
+            while i - deque[0] > k-1: # 如果相距超过窗口k长度则弃掉
+                deque.pop(0)
+            if i >= k - 1:
+                result.append(nums[deque[0]]) # 这过程中始终保持deque[0]为最大值的index
+        return result
