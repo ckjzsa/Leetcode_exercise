017cda82323ff708308a032080681b5bdae452dc
Kehua Chen <48197101+ckjzsa@users.noreply.github.com>
2020-07-05 19:24:12 +0800

    Create *53. 0-n-1中缺失的数字.py

---
+++ 剑指offer/*53. 0-n-1中缺失的数字.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        i, j = 0, len(nums) - 1
+        while i <= j:
+            m = (i + j) // 2
+            if nums[m] > m:
+                j = m - 1 
+            else:
+                i = m + 1
+
+        return i
