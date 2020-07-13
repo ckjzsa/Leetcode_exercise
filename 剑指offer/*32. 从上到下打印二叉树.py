de871216ce2bb7d02c2351357aa304345431c8d0
Kehua Chen <48197101+ckjzsa@users.noreply.github.com>
2020-07-13 17:06:55 +0800

    Create *32. 从上到下打印二叉树.py

---
+++ 剑指offer/*32. 从上到下打印二叉树.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        
+        res = []
+        queue = []
+        queue.append(root)
+
+        while queue:
+            node = queue.pop(0)
+            res.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        
+        return res
