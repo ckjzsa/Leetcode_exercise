6602fd486ec359e28d77839b4d12ff81a6c6a982
Kehua Chen <48197101+ckjzsa@users.noreply.github.com>
2020-07-17 17:21:55 +0800

    Create *31. 栈的压入、弹出序列.py

---
+++ 剑指offer/*31. 栈的压入、弹出序列.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def validateStackSequences(self, pushed, popped):
+        """
+        :type pushed: List[int]
+        :type popped: List[int]
+        :rtype: bool
+        """
+        stack = []
+        i = 0
+        
+        for num in pushed:
+            stack.append(num)
+            while stack and stack[-1] == popped[i]:
+                stack.pop()
+                i += 1
+        
+        return not stack
